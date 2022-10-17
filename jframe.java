a498d8ea02034564845c05075f315824dbaecdf1
Arman <armanshaikh67868282@gmail.com>
2022-10-17 21:43:45 +0530

    adding all the files in local repo

---
+++ jframe.java
@@ -0,0 +1,18 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class jframe {
+ public static void main(String[]args){
+  new fist();
+ }
+}
+
+class fist extends JFrame{
+ public void fist(){
+ setVisible(true);
+ setSize(400,400);
+ setLayout(new FlowLayout());
+}
+}
+
